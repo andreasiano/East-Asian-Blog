57bde1754fa999a2bca44a073b4ce5c1b81f4a87
Kungfu90 <andrea.siano@studio.unibo.it>
2023-06-07 20:01:25 +0200

    Adding New Website

---
+++ Korea/assets/js/main.js
@@ -0,0 +1,30 @@
+const navEl = document.querySelector('.navbar');
+const brandEl = document.querySelector('.brand-style')
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY >= 56) {
+        navEl.classList.add('custom-nav')
+        brandEl.classList.add('custom-brand')
+        
+    } else if (window.scrollY < 56) {
+        navEl.classList.remove('custom-nav')
+        brandEl.classList.remove("custom-brand");
+    }
+})
+
+$(".owl-carousel").owlCarousel({
+  loop: true,
+  margin: 10,
+  nav: true,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 3,
+    },
+    1000: {
+      items: 4,
+    },
+  },
+});
